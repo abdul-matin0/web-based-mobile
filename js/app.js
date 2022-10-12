0ad5484b92719ee7c02280d7cdce0eac8d67ea98
Abdul-Matin Adedeji Adebayo <AA5328@live.mdx.ac.uk>
2022-10-12 16:54:39 +0100

    implement vue layout to index

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 const app = new Vue({
     el: '#app',
     data: {
-        
+        products: [
+            {
+                subject: '',
+                location: '',
+                price: 100,
+                spaces: 5
+            },
+
+            {
+                subject: '',
+                location: '',
+                price: 100,
+                spaces: 15
+            }
+        ]
     }
 });
