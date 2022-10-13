058806ea98017dd2dcaf49291301cbb4ecd8c43e
Abdul-matin <madeayo04@gmail.com>
2022-10-13 14:56:44 +0100

    dynamic display on index

--- js/app.js
+++ js/app.js
@@ -2,19 +2,20 @@ const app = new Vue({
     el: '#app',
     data: {
         products: [
-            {
-                subject: '',
-                location: '',
-                price: 100,
-                spaces: 5
-            },
-
-            {
-                subject: '',
-                location: '',
-                price: 100,
-                spaces: 15
-            }
+            { id: 1, subject: 'Math', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
+            { id: 2, subject: 'English', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
+            { id: 3, subject: 'Chemistry', location: 'Dubai', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
+            { id: 4, subject: 'Physics', location: 'Manchester', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
+            { id: 5, subject: 'Biology', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 }
         ]
+    },
+    methods: {
+        addToCart(id){
+            console.log(id, id.type);
+        },
+
+        increasePrice(){
+            this.products[0].price = ++this.products[0].price;
+        }
     }
 });
