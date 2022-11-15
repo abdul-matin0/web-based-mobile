7e603c61d4349d60575f0207018eb20227f1cf87
Abdul-matin <madeayo04@gmail.com>
2022-11-15 21:11:00 +0100

    display fix

--- js/lessons.js
+++ js/lessons.js
@@ -1,8 +1,8 @@
 // list of lessons
 var lessons = [
-    { id: 1, subject: 'Math', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
-    { id: 2, subject: 'English', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
-    { id: 3, subject: 'Chemistry', location: 'Dubai', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
-    { id: 4, subject: 'Physics', location: 'Manchester', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 },
-    { id: 5, subject: 'Biology', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5 }
+    { id: 1, subject: 'Math', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5, icon: 'fas fa-calculator' },
+    { id: 2, subject: 'English', location: 'London', imageURL: 'assets/subject.jpg', altText: 'english image', price: 100, spaces: 5, icon: 'fas fa-book-open' },
+    { id: 3, subject: 'Chemistry', location: 'Dubai', imageURL: 'assets/subject.jpg', altText: 'chemistry image', price: 100, spaces: 5, icon: 'fas fa-atom' },
+    { id: 4, subject: 'Physics', location: 'Hull', imageURL: 'assets/subject.jpg', altText: 'physics image', price: 100, spaces: 5, icon: 'fas fa-tools' },
+    { id: 5, subject: 'Biology', location: 'London', imageURL: 'assets/subject.jpg', altText: 'biology image', price: 100, spaces: 5, icon: 'fas fa-user-md' }
 ];
