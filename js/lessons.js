415e21ae26cc1343749ca9720418301695c3e4c3
Abdul-matin <madeayo04@gmail.com>
2022-11-16 20:47:01 +0100

    adding more lessons

--- js/lessons.js
+++ js/lessons.js
@@ -1,8 +1,13 @@
 // list of lessons
 var lessons = [
-    { id: 1, subject: 'Math', location: 'London', imageURL: 'assets/subject.jpg', altText: 'math image', price: 100, spaces: 5, icon: 'fas fa-calculator' },
-    { id: 2, subject: 'English', location: 'London', imageURL: 'assets/subject.jpg', altText: 'english image', price: 200, spaces: 5, icon: 'fas fa-book-open' },
-    { id: 3, subject: 'Chemistry', location: 'Dubai', imageURL: 'assets/subject.jpg', altText: 'chemistry image', price: 300, spaces: 5, icon: 'fas fa-atom' },
-    { id: 4, subject: 'Physics', location: 'Hull', imageURL: 'assets/subject.jpg', altText: 'physics image', price: 100, spaces: 5, icon: 'fas fa-tools' },
-    { id: 5, subject: 'Biology', location: 'London', imageURL: 'assets/subject.jpg', altText: 'biology image', price: 150, spaces: 5, icon: 'fas fa-user-md' }
+    { id: 1, subject: 'Math', location: 'London', imageURL: 'assets/math.jpg', altText: 'math image', price: 100, spaces: 5, icon: 'fas fa-calculator' },
+    { id: 2, subject: 'English', location: 'Lagos', imageURL: 'assets/english.jpg', altText: 'english image', price: 200, spaces: 5, icon: 'fas fa-book-open' },
+    { id: 3, subject: 'Chemistry', location: 'Dubai', imageURL: 'assets/chemistry.jpg', altText: 'chemistry image', price: 300, spaces: 5, icon: 'fas fa-atom' },
+    { id: 4, subject: 'Physics', location: 'Hull', imageURL: 'assets/physics.jpg', altText: 'physics image', price: 100, spaces: 5, icon: 'fas fa-tools' },
+    { id: 5, subject: 'Biology', location: 'Leeds', imageURL: 'assets/biology.jpg', altText: 'biology image', price: 150, spaces: 5, icon: 'fas fa-user-md' },
+    { id: 6, subject: 'Literature', location: 'London', imageURL: 'assets/literature.jpg', altText: 'literature image', price: 100, spaces: 5, icon: 'fas fa-calculator' },
+    { id: 7, subject: 'Geography', location: 'Fulham', imageURL: 'assets/geography.jpg', altText: 'geography image', price: 200, spaces: 5, icon: 'fas fa-book-open' },
+    { id: 8, subject: 'Hospitality', location: 'Dubai', imageURL: 'assets/hospitality.jpg', altText: 'hospitality image', price: 300, spaces: 5, icon: 'fas fa-atom' },
+    { id: 9, subject: 'Nutrition', location: 'Hendon', imageURL: 'assets/nutrition.jpg', altText: 'nutrition image', price: 100, spaces: 5, icon: 'fas fa-tools' },
+    { id: 10, subject: 'Law', location: 'London', imageURL: 'assets/law.jpg', altText: 'law image', price: 150, spaces: 5, icon: 'fas fa-user-md' }
 ];
